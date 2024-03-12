f3f9cf57a962559c177921c987896dc08d9dec50
Dzhessielis <162979338+Dzhessielis@users.noreply.github.com>
2024-03-12 19:38:57 +0300

    PlayerController
    Все базовые механики движения и гравитация

--- Assets/Player Character/Scripts/PlayerController.cs
+++ Assets/Player Character/Scripts/PlayerController.cs
@@ -4,15 +4,58 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float _fallVelocity = 0;
+    public float gravity = 9.8f;
+    private CharacterController _characterController;
+    public float jumpForce;
+    public float speed;
+    private Vector3 _moveVector;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+
+        _moveVector = Vector3.zero;
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+
+        if(Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+
+        if(Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)&&_characterController.isGrounded)
+        {
+            _fallVelocity = -jumpForce;
+        }
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        _characterController.Move(_moveVector * speed * Time.fixedDeltaTime);
+        _fallVelocity += gravity * Time.fixedDeltaTime;
+        _characterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
+
+        if (_characterController.isGrounded)
+            _fallVelocity = 0;
+
     }
 }
