41ad764b7825eb141ef92ec9efd935d62d5aa5ef
Dzhessielis <162979338+Dzhessielis@users.noreply.github.com>
2024-03-13 13:31:23 +0300

    Update EnemyAI.cs

--- Assets/Player Character/Scripts/EnemyAI.cs
+++ Assets/Player Character/Scripts/EnemyAI.cs
@@ -5,17 +5,20 @@ using UnityEngine.AI;
 
 public class EnemyAI : MonoBehaviour
 {
-    public Transform targetPoint;
+    public List<Transform> patrolPoints;
     private NavMeshAgent _navMeshAgent;
     // Start is called before the first frame update
     void Start()
     {
         _navMeshAgent = GetComponent<NavMeshAgent>();
+
+       // Random.Range(patrolPoints);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        _navMeshAgent.destination = targetPoint.position;
+       // _navMeshAgent.destination = targetPoint.position;
     }
 }
